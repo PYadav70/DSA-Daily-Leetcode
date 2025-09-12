975be585c6e2cdf3a11daa49ba024b2a552e98a8
Pappu kr Yadav <166101530+PYadav70@users.noreply.github.com>
2025-09-12 23:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (80.90%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+     vector<vector<int>>result;
+     void solve(int i ,vector<int>&nums ,vector<int>& temp ){
+        if(i>=nums.size()){
+            result.push_back(temp);
+            return ;
+        }
+        temp.push_back(nums[i]); //take ith element
+        solve( i+1,nums, temp);
+        temp.pop_back();// not take the ith element
+        solve( i+1,nums, temp);
+
+     }
+    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<int>temp; //store the subset
+
+    solve(0,nums, temp );
+       
+    
+
+    return result;
+        
+    }
+};
