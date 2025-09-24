59e03cbfd3f578bcd9c9ddee18d2fcb034dced0a
Pappu kumar Yadav <166101530+PYadav70@users.noreply.github.com>
2025-09-25 00:51:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (73.95%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(int i=0 ; i<s.length(); i++){
+            char ch = s[i];
+            //opening
+            if(ch == '('|| ch=='{'|| ch=='['){
+                st.push(ch);
+            }else{
+              //closing
+              if(!st.empty()){
+                char topCh = st.top();
+                if(ch == ')' && topCh == '('){
+                    st.pop();
+                }
+                 else if(ch == '}' && topCh == '{'){
+                    st.pop();
+                }
+                else if(ch == ']' && topCh == '['){
+                    st.pop();
+                }else{
+                    return false;
+                }
+
+              } else{
+
+               return false;
+              }
+            }
+            
+            
+        }
+         //check stack
+         if(st.empty()){
+            return true;
+         }else{
+            return false;
+         }
+        
+    }
+};
