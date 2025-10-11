2f80853257d2605b0ec430bca8a9d0bc429650f9
Pappu kumar Yadav <166101530+PYadav70@users.noreply.github.com>
2025-10-11 17:09:05 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (83.76%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -10,32 +10,45 @@
  * };
  */
 class Solution {
-    private:
-    int height(TreeNode* node){
-        if(node == NULL){
-            return 0;
-        }
-        int left = height(node->left);
-        int right = height(node->right);
+    // private:
+    // int height(TreeNode* node){
+    //     if(node == NULL){
+    //         return 0;
+    //     }
+    //     int left = height(node->left);
+    //     int right = height(node->right);
 
-        int ans = max(left, right)+1;
-        return ans;
-    }
+    //     int ans = max(left, right)+1;
+    //     return ans;
+    // }
 public:
-    bool isBalanced(TreeNode* root) {
-        if(root == NULL){
-            return true;
+    pair <bool, int> isBalancedFast(TreeNode* root){
+         if(root == NULL){
+           pair <bool, int> p = make_pair(true,0);
+           return p;
         }
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
+        pair<bool, int> left = isBalancedFast(root->left);
+        pair<bool, int> right = isBalancedFast(root->right);
 
-        bool diff = abs(height(root->left)- height(root->right)) <=1;
+        bool leftAns = left.first;
+        bool rightAns = right.first;
 
-        if(left && right && diff){
-            return true;
+        bool diff = abs(left.second- right.second) <=1;
+
+        pair<bool, int>ans;
+        ans.second = max(left.second, right.second) +1; 
+        if(leftAns && rightAns && diff){
+        ans.first = true;
+
         }else{
-            return false;
+            ans.first = false;;
         }
-        
-    }
+        return ans;
+     }
+
+   
+    bool isBalanced(TreeNode* root) {
+       return isBalancedFast(root).first;
+
+     }   
 };
