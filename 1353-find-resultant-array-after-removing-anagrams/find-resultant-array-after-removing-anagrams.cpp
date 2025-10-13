31dd0c625145881950963b82761702021717d682
Pappu kumar Yadav <166101530+PYadav70@users.noreply.github.com>
2025-10-13 09:58:02 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (96.97%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,35 +1,53 @@
 class Solution {
 public:
-    bool checkAnagrams(string &s1, string &s2){
-        int arr[26] = {0};
+    // bool checkAnagrams(string &s1, string &s2){
+    //     int arr[26] = {0};
 
-        for(char &ch : s1){
-            arr[ch -'a']++;
-        }
-          for(char &ch : s2){
-            arr[ch-'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(arr[i] != 0){
-                return false;
-            }
-        }
-        return true;
-    }
+    //     for(char &ch : s1){
+    //         arr[ch -'a']++;
+    //     }
+    //       for(char &ch : s2){
+    //         arr[ch-'a']--;
+    //     }
+    //     for(int i=0; i<26; i++){
+    //         if(arr[i] != 0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     vector<string> removeAnagrams(vector<string>& words) {
-        int n = words.size();
-        vector<string>result;
+    //     int n = words.size();
+    //     vector<string>result;
 
-        result.push_back(words[0]);
+    //     result.push_back(words[0]);
 
-        for(int i=1; i<n; i++){
-            if(!checkAnagrams(words[i], result.back())){
-                result.push_back(words[i]);
-            }
+    //     for(int i=1; i<n; i++){
+    //         if(!checkAnagrams(words[i], result.back())){
+    //             result.push_back(words[i]);
+    //         }
             
 
-        }
+    //     }
 
-       return result;
+    //    return result;
+
+    int n = words.size();
+    vector<string>result;
+
+    result.push_back(words[0]);
+
+    for(int i=1; i<n; i++){
+        string prevWords = words[i-1];
+        string currWords = words[i];
+
+        sort(prevWords.begin(), prevWords.end());
+        sort(currWords.begin(), currWords.end());
+
+        if(prevWords != currWords){
+            result.push_back(words[i]);
+        }
+    }
+    return result;
     }
 };
