3a46bcca6f0ac35d6dbbfcd8a199a83fe1ed7c93
Pappu kumar Yadav <166101530+PYadav70@users.noreply.github.com>
2025-11-19 00:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (86.02%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        stack<int>st;
+        vector<string>result;
+        int i = 0;
+
+        for(int num : target){
+            while(i<num-1){
+                result.push_back("Push");
+                result.push_back("Pop");
+                i++;
+            }
+            result.push_back("Push");
+            i++;
+        }
+        return result;    
+    }
+};
