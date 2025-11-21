c0a78433d3fe08beb19282f31c3028c30178faa6
Pappu kumar Yadav <166101530+PYadav70@users.noreply.github.com>
2025-11-21 20:06:08 +0530

    Time: 183 ms (35.06%) | Memory: 16 MB (58.20%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int n = s.length();
+        unordered_set<char>words;
+
+        for(int i = 0; i<n ; i++){
+            words.insert(s[i]);
+        }
+
+        int result = 0;
+
+        for(char word : words ){
+
+         int left_idx = -1;
+         int right_idx = -1;
+
+         for(int i = 0; i<n; i++){
+            if(s[i] == word){
+                if(left_idx == -1){
+                    left_idx = i;
+                }
+                right_idx = i;
+            }
+         }
+         unordered_set<char>st;
+         for(int middle = left_idx+1; middle <= right_idx-1; middle++){
+            st.insert(s[middle]);
+         }
+         result += st.size();
+
+        }
+        return result;
+    }
+};
