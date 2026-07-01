03c38bd6c2ad6066ae1f50b829923cec5e472466
Pappu kumar Yadav <166101530+PYadav70@users.noreply.github.com>
2026-07-01 23:18:12 +0530

    Time: 12 ms (43.58%) | Memory: 11.9 MB (53.67%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -12,23 +12,17 @@ public:
         if(head == NULL || head->next == NULL){
             return false;
         }
-        ListNode* slow = head;
-        ListNode* fast = head;
+       ListNode* slow = head;
+       ListNode* fast = head;
 
-        while(fast !=NULL && fast->next !=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
+       while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
 
-            if(slow== fast){
-                return true;
-            }
-
+        if(slow == fast){
+            return true;
         }
-       
-
-        
-        return false;
-
-   
+       }
+       return false;
     }
 };
