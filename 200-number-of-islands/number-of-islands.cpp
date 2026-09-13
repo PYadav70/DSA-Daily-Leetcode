f8a877d5892abd5d937e1398441b2ffe1cc93906
Pappu kumar Yadav <166101530+PYadav70@users.noreply.github.com>
2026-09-13 23:28:26 +0530

    Time: 27 ms (55.10%) | Memory: 16.1 MB (90.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void dfs(vector<vector<char>>& grid, int i, int j){
         int row = grid.size();
-        int col =  grid[0].size();
+        int col = grid[0].size();
 
-        if(i < 0 || j < 0 || i >= row || j >= col || grid[i][j] == '0'){
+        if(i<0 || j<0 || i>=row || j>=col || grid[i][j] == '0'){
             return ;
         }
         grid[i][j] = '0';
@@ -18,15 +18,16 @@ public:
         int row = grid.size();
         int col = grid[0].size();
 
-        int count = 0;
+        int islandCount = 0;
 
-        for(int i=0; i<row; i++){
-            for(int j = 0; j<col; j++)
-            if(grid[i][j] == '1'){
-                count++;
-                dfs(grid, i, j);
+        for(int i = 0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(grid[i][j] == '1'){
+                    islandCount++;
+                    dfs(grid, i, j);
+                }
             }
         }
-        return count;
+        return islandCount;
     }
 };
