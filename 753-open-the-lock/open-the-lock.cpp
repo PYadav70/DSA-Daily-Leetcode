3887ac2218a6f608d1f9b1c1cf94dd8b178b2036
Pappu kumar Yadav <166101530+PYadav70@users.noreply.github.com>
2026-09-21 22:28:31 -0700

    Time: 87 ms (95.34%) | Memory: 39.3 MB (56.83%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void fillNeighbors(queue<string>&que, string &curr, unordered_set<string>&st){
+        for(int i=0; i<4; i++){
+            char ch = curr[i];
+            char desc = ch=='0' ? '9' : ch-1;
+            char inc = ch=='9' ? '0' : ch+1;
+
+            curr[i] = desc;
+            if(st.find(curr) == st.end()){
+                st.insert(curr);
+                que.push(curr);
+            }
+            curr[i] = inc;
+            if(st.find(curr) == st.end()){
+                st.insert(curr);
+                que.push(curr);
+            }
+            curr[i] = ch;
+        }
+    }
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string>st(begin(deadends), end(deadends));
+        string start = "0000";
+        int levelCount = 0;
+
+        if(st.find(start) != st.end()){
+            return -1;
+        }
+        queue<string>que;
+        que.push(start);
+
+        while(!que.empty()){
+            int n = que.size();
+
+            while(n--){
+                string curr = que.front();
+                que.pop();
+
+                if(curr == target){
+                    return levelCount;
+                }
+                fillNeighbors(que, curr, st);
+            }
+             levelCount++;
+        }
+        return -1;
+    }
+};
